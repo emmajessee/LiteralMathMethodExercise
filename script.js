6174be1b9d409e8189816a821537548a5585fb0f
Emma Jessee <emma.jessee@stu.glasgow.kyschools.us>
2024-02-01 11:03:02 -0600

    LiteralMathMethodExercise

---
+++ script.js
@@ -0,0 +1,31 @@
+//1a. 
+let warmHugs ="Hi, I'm Olaf and I like warm hugs.";
+
+//1b.
+console.log(warmHugs);
+
+//1c. 
+console.log(warmHugs.replace( "like", "love"));
+warmHugs = "Hi, I'm Olaf and I love warm hugs.";
+
+//2a. 
+var beenImpaled = "Oh, look at that. I've been impaled."
+
+//2b. 
+console.log(beenImpaled.slice(17, 36));
+
+//3a.
+var dotDotDot = "..."
+
+//3b.
+var skullBones = `I don't have a skull${dotDotDot} or bones.`
+console.log(skullBones);
+
+//4. 
+console.log(Math.PI);
+
+//5. 
+let randomNumber = Math.random();
+randomNumber *=3;
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
